06ada97cab6a19cfe656601d4050537cd1925520
Tymofii <tymofiimustetsov@gmail.com>
2019-11-14 12:43:21 +0200

    v

---
+++ src/app/device-categories/device-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-categories',
+  templateUrl: './device-categories.component.html',
+  styleUrls: ['./device-categories.component.scss']
+})
+export class DeviceCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
