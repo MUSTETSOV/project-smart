59ac13f31a57ddd262c57a51c4dd160a7bb03db6
Tymofii <tymofiimustetsov@gmail.com>
2019-11-18 16:21:24 +0200

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './shared/header/header.component';
 import { IntroComponent } from './intro/intro.component';
 import { AboutusComponent } from './main/aboutus/aboutus.component';
@@ -31,6 +31,8 @@ import { BrandsService } from './services/brands.service';
 import { BuyPartsComponent } from './buy-parts/buy-parts.component';
 import { ModelCardComponent } from './buy-parts/model-card/model-card.component';
 
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 
@@ -51,6 +53,7 @@ import { ModelCardComponent } from './buy-parts/model-card/model-card.component'
     DevicesComponent,
     BuyPartsComponent,
     ModelCardComponent
+
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'smartfix-app' }),
@@ -73,7 +76,11 @@ import { ModelCardComponent } from './buy-parts/model-card/model-card.component'
       // { path: 'admin/products', component: AdminProductsComponent },
       // { path: 'admin/orders', component: AdminOrdersComponent }
     ]),
-    HttpClientModule
+    HttpClientModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
+
   ],
   providers: [BrandsService],
   bootstrap: [AppComponent]
--- src/app/buy-parts/buy-parts.component.ts
+++ src/app/buy-parts/buy-parts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-buy-parts',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buy-parts.component.scss']
 })
 export class BuyPartsComponent implements OnInit {
+
+  toppings = new FormControl();
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
   constructor() { }
 
